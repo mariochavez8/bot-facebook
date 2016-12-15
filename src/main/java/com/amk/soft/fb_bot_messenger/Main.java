f4e8459062dabac761e19305d54add8417208570
mario.chavez <mario.chavez@amk-technologies.com>
2016-12-15 11:42:25 -0600

    Add new Actions

--- src/main/java/com/amk/soft/fb_bot_messenger/Main.java
+++ src/main/java/com/amk/soft/fb_bot_messenger/Main.java
@@ -51,9 +51,11 @@ public class Main {
             for (Messaging messaging : messagings) {
                 String senderId = messaging.sender.id;
                 if (messaging.message != null) {
+                    Message.Text("1").sendTo(senderId);
                     sendMenuMessage(senderId,
                                     "Menu...");
                 } else if (messaging.postback.payload != null) {
+                    Message.Text("2").sendTo(senderId);
                     switch (messaging.postback.payload) {
                         case USER_DEFINED_PAYLOAD:
                             sendMenuMessage(senderId,
